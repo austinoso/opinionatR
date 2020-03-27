e152e1374b32f12787b7c72f961f138564afad3d
austinoso <osorio_austin@yahoo.com>
2020-03-27 09:35:34 -0700

    added ResponseSerializer

--- app/serializers/response_serializer.rb
+++ app/serializers/response_serializer.rb
@@ -1,3 +1,7 @@
 class ResponseSerializer < ActiveModel::Serializer
   attributes :id, :content, :user
+
+  def user
+    ActiveModel::SerializableResource.new(object.user,  each_serializer: UserSerializer)
+  end
 end
