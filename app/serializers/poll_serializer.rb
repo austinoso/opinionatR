7cc959f8935bb66183a57f2c5a913aa687e08b9a
austinoso <osorio_austin@yahoo.com>
2020-03-27 09:37:58 -0700

    showing users on polls

--- app/serializers/poll_serializer.rb
+++ app/serializers/poll_serializer.rb
@@ -1,3 +1,7 @@
 class PollSerializer < ActiveModel::Serializer
   attributes :id, :company, :question, :responses
+
+  def responses
+    ActiveModel::SerializableResource.new(object.responses,  each_serializer: ResponseSerializer)
+  end
 end
